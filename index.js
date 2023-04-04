874a3818866fdb29c9bda456f5a33fd2c2f971fc
karolis879 <58052007+karolis879@users.noreply.github.com>
2023-04-04 16:34:09 +0300

    Function intro

--- index.js
+++ index.js
@@ -62,3 +62,26 @@ console.log(treciasSarasas[sarasuVertes]);
 console.log(ketvirtasSarasas[4] + ', ', ketvirtasSarasas[3] + ', ', ketvirtasSarasas[2] + ', ', ketvirtasSarasas[1] + ', ', ketvirtasSarasas[0]);
 console.log(penktasSarasas[4] + ', ', penktasSarasas[3] + ', ', penktasSarasas[2] + ', ', penktasSarasas[1] + ', ', penktasSarasas[0]);
 console.log(sestasSarasas[4] + ', ', sestasSarasas[3] + ', ', sestasSarasas[2] + ', ', sestasSarasas[1] + ', ', sestasSarasas[0]);
+
+//---Function------------
+
+function tusciaFunkcija () {
+    return false
+}
+
+console.log(tusciaFunkcija())
+
+//-----------------------
+
+function daugyba (a, b) {
+    const sandauga =  a * b;
+    return sandauga;
+}
+
+const skaicius1 = 2;
+const skaicius2 = 3;
+const skaicius3 = 4;
+
+console.log( daugyba( skaicius1, skaicius2 ) );
+console.log( daugyba( skaicius3, skaicius2 ) );
+console.log( daugyba( skaicius1, skaicius3 ) );
